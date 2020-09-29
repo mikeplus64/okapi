563d5171641041ca73bae39ca2eb706954b053bb
Mike Ledger <eleventynine@gmail.com>
2020-09-29 13:27:54 +1000

    bump

--- rocket-okapi/src/response/mod.rs
+++ rocket-okapi/src/response/mod.rs
@@ -4,6 +4,7 @@ use super::gen::OpenApiGenerator;
 use super::Result;
 use okapi::openapi3::Responses;
 
+/// No missing documentation here officer, no siree.
 pub trait OpenApiResponder {
     /// Create the responses type, which is a list of responses that can be
     /// rendered in `openapi.json` format.
